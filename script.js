8c25dc9a3170d29a4fa7fe90a5bc83270bba3a68
Daryna-g <gickad@gmail.com>
2025-07-08 21:10:26 +0300

    Initial commiy

---
+++ script.js
@@ -0,0 +1,21 @@
+const form = document.querySelector("form");
+const ratingCard = document.querySelector(".rating-card");
+const thanksCard = document.querySelector(".thanks-card");
+const submitBtn = document.querySelector(".btn");
+const rate = document.getElementById("rate");
+const errorMessage = document.querySelector(".error-message");
+
+function showCard(e) {
+	e.preventDefault();
+	let selectedRating = form.radio.value;
+
+	if (!selectedRating) {
+		errorMessage.style.display = "block";
+		return;
+	}
+	rate.textContent = selectedRating;
+	ratingCard.style.display = "none";
+	thanksCard.classList.remove("hidden");
+}
+
+form.addEventListener("submit", showCard);
